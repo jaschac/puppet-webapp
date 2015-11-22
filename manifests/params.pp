9cdb97659d17948b2d00ecf43b6852415fbd37e3
jascha <jaschacasadio@gmail.com>
2015-11-22 05:40:46 +0100

    Added Nginx PHP-FPM EPP template

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,4 +2,23 @@
 class webapp::params
   (
     String[1,default] $language,
+
+    Optional[
+      Hash[
+        String,
+	Struct[{
+          language => Enum['html', 'php', 'python'],
+	  vcs      => Enum['git'],
+	  ws       => Struct[{
+	    engine        => Enum['apache', 'nginx'],
+	    template      => Enum['php-fpm'],
+	    template_args => Struct[{
+	      listen_port => String[1, default],
+	      root_dir    => String[1, default],
+	      server_name => String[1, default]
+	      }] 
+	    }]
+	}]
+      ]
+    ] $webapps,
 ){}
